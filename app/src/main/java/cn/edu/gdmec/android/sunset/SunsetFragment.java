fadda2d32c55833ca329cbbe30e75507de6df929
huangguangda <2397923107@qq.com>
2017-11-27 15:03:31 +0800

    特效添加成功

--- app/src/main/java/cn/edu/gdmec/android/sunset/SunsetFragment.java
+++ app/src/main/java/cn/edu/gdmec/android/sunset/SunsetFragment.java
@@ -1,16 +1,23 @@
 package cn.edu.gdmec.android.sunset;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
 
 /**
  * Created by Jack on 2017/11/27.
  */
 
 public class SunsetFragment extends Fragment{
+    private View mSceneView;
+    private View mSunView;
+    private View mSkyView;
+
     public static SunsetFragment newInstance(){
         return new SunsetFragment ();
     }
@@ -18,6 +25,29 @@ public class SunsetFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
         View view = inflater.inflate ( R.layout.fragment_sunset, container, false );
+
+        mSceneView = view;
+        mSunView = view.findViewById ( R.id.sum );
+        mSkyView = view.findViewById ( R.id.sky );
+
+        mSceneView.setOnClickListener ( new View.OnClickListener (){
+            @Override
+            public void onClick(View v){
+                startAnimation ();
+            }
+        } );
         return view;
+    }
+
+    private void startAnimation(){
+        float sunYStart = mSunView.getTop ();
+        float sumYEnd = mSkyView.getHeight ();
+
+        ObjectAnimator heightAnimator = ObjectAnimator
+                .ofFloat ( mSunView, "y", sunYStart, sumYEnd )
+                .setDuration(3000);
+        //添加特效
+        heightAnimator.setInterpolator ( new AccelerateInterpolator (  ) );
+        heightAnimator.start ();
     }
 }

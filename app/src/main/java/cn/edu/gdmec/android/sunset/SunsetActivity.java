c14f87faf97f2ee36e4b16fa7e57144055878a10
huangguangda <2397923107@qq.com>
2017-11-27 14:53:41 +0800

    哈哈哈哈ok

--- app/src/main/java/cn/edu/gdmec/android/sunset/SunsetActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/sunset/SunsetActivity.java
@@ -1,13 +1,18 @@
 package cn.edu.gdmec.android.sunset;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SunsetActivity extends AppCompatActivity {
-
+public class SunsetActivity extends SingleFragmentActivity {
     @Override
+    protected Fragment createFragment(){
+        return SunsetFragment.newInstance ();
+    }
+
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_sunset );
-    }
+    }*/
 }
